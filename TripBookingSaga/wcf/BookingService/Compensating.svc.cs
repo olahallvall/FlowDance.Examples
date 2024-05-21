a8fa5937ee119fa8fe4268e9b7c8a611ff246776
Ola Hällvall <ola.hallvall@gmail.com>
2024-05-21 13:08:48 +0200

    Update Compensating.svc.cs

--- TripBookingSaga/wcf/BookingService/Compensating.svc.cs
+++ TripBookingSaga/wcf/BookingService/Compensating.svc.cs
@@ -4,14 +4,36 @@ namespace BookingService
 {
     public class Compensating : ICompensating
     {
-        public void Compensate(string postData)
+        public void Compensate()
         {
             var loggerFactory = LoggerFactory.Create(builder =>
             {
                 builder.AddConsole();
             });
 
+             var json = GetJsonFromBody();
             // "Rollback" to a good state :)
         }
+    }
+
+    private string GetJsonFromBody()
+    {
+        string json = "";
+        string contentType = WebOperationContext.Current.IncomingRequest.ContentType;
+        if (contentType.Contains("application/json"))
+        {
+            var requestMessage = OperationContext.Current.RequestContext.RequestMessage;
+            var messageDataProperty = requestMessage.GetType().GetProperty("MessageData", (BindingFlags)0x1FFFFFF);
+            var messageData = messageDataProperty.GetValue(requestMessage);
+            var bufferProperty = messageData.GetType().GetProperty("Buffer");
+            var buffer = bufferProperty.GetValue(messageData) as ArraySegment<byte>?;
+            json = Encoding.UTF8.GetString(buffer.Value.Array);
+        }
+        else if (contentType.Contains("text"))
+        {
+            json = Encoding.UTF8.GetString(OperationContext.Current.RequestContext.RequestMessage.GetBody<byte[]>());
+        }
+    
+        return json;
     }
 }
