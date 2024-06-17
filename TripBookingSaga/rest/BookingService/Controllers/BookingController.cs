c64c7e1929ce12a0b28c36c3ad48a144cd951dde
Ola Hällvall <ola.hallvall@gmail.com>
2024-06-17 23:04:29 +0200

    Using FlowDance.Client.AspNetCore

--- TripBookingSaga/rest/BookingService/Controllers/BookingController.cs
+++ TripBookingSaga/rest/BookingService/Controllers/BookingController.cs
@@ -1,7 +1,7 @@
 using BookingService.Models;
 using BookingService.Services;
 using FlowDance.Client;
-using FlowDance.Common.CompensatingActions;
+using FlowDance.Client.AspNetCore.ActionFilters;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookingService.Controllers
@@ -19,32 +19,31 @@ namespace BookingService.Controllers
             _carService = carService;
         }
 
+        [CompensationSpan(CompensatingActionUrl = "http://localhost:5112/api/Compensating/compensate", CompensationSpanOption = CompensationSpanOption.RequiresNew)]
         [HttpPost("booktrip")]
         public async Task<IActionResult> BookTrip([FromBody] Trip trip)
         {
             var traceId = Guid.NewGuid();
+
+            // Access the CompensationSpan instance from the ActionFilter
+            var compensationSpan = HttpContext.Items["CompensationSpan"] as CompensationSpan;
             
-            using (var compSpan = new CompensationSpan(new HttpCompensatingAction("http://localhost:5112/api/Compensating/compensate"), traceId, _loggerFactory))
-            {
-                using var db = new TripContext();
+            using var db = new TripContext();
        
-                db.Add(trip);
+            db.Add(trip);
 
-                //db.ChangeTracker.DetectChanges();
-                //var shortView = db.ChangeTracker.DebugView.ShortView;
-                //var longView = db.ChangeTracker.DebugView.LongView;
+            //db.ChangeTracker.DetectChanges();
+            //var shortView = db.ChangeTracker.DebugView.ShortView;
+            //var longView = db.ChangeTracker.DebugView.LongView;
 
-                await db.SaveChangesAsync();
+            await db.SaveChangesAsync();
 
-                //compSpan.AddCompensationData(shortView, "ShortView");
-                //compSpan.AddCompensationData(longView, "LongView");
-                compSpan.AddCompensationData(trip.TripId.ToString(), "TripId");
+            //compSpan.AddCompensationData(shortView, "ShortView");
+            //compSpan.AddCompensationData(longView, "LongView");
+            compensationSpan.AddCompensationData(trip.TripId.ToString(), "TripId");
 
-                // Book a Car
-                await _carService.BookCar(trip.PassportNumber, trip.TripId, traceId);
-
-                compSpan.Complete();
-            }
+            // Book a Car
+            await _carService.BookCar(trip.PassportNumber, trip.TripId, traceId);
 
             return Ok(trip);
         }
